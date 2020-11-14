cae071304d064fffd65c8a98337226715b13eb7e
Eros Borba <borba.eros@gmail.com>
2020-11-13 22:35:38 -0300

    add the 'add' function

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_personal_expenses/txTile.dart';
 
 import 'transaction.dart';
@@ -33,71 +34,74 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     String titleInput;
-    String amountInput;
+    double amountInput;
 
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Column(
-        children: [
-          Container(
-            width: double.infinity,
-            child: Card(
-              elevation: 8,
-              child: Text('Teste'),
-            ),
-          ),
-          Card(
-            elevation: 5,
-            child: Container(
-              padding: EdgeInsets.all(10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  TextField(
-                    onChanged: (text) {
-                      titleInput = text;
-                    },
-                    decoration: InputDecoration(labelText: 'Item'),
-                  ),
-                  TextField(
-                    keyboardType: TextInputType.number,
-                    onChanged: (text) {
-                      amountInput = text;
-                    },
-                    decoration: InputDecoration(labelText: 'Valor'),
-                  ),
-                  FlatButton(
-                    child: Text('Adicionar'),
-                    color: Colors.purple,
-                    textColor: Colors.white,
-                    onPressed: () {
-                      setState(() {
-                        transactions.add(Transaction(
-                          id: DateTime.now().toString(),
-                          title: titleInput,
-                          amount: null,
-                          timestamp: DateTime.now(),
-                        ));
-                      });
-                    },
-                  ),
-                ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              child: Card(
+                elevation: 8,
+                child: Text('Teste'),
               ),
             ),
-          ),
-          Column(
-            children: transactions
-                .map((tx) => TxCard(
-                      id: tx.id,
-                      title: tx.title,
-                      amount: tx.amount,
-                      timestamp: tx.timestamp,
-                    ))
-                .toList(),
-          )
-        ],
+            Card(
+              elevation: 5,
+              child: Container(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    TextField(
+                      onChanged: (text) {
+                        titleInput = text;
+                      },
+                      decoration: InputDecoration(labelText: 'Item'),
+                    ),
+                    TextField(
+                      keyboardType: TextInputType.number,
+                      onChanged: (text) {
+                        amountInput = double.parse(text);
+                      },
+                      decoration: InputDecoration(labelText: 'Valor'),
+                    ),
+                    FlatButton(
+                      child: Text('Adicionar'),
+                      color: Colors.purple,
+                      textColor: Colors.white,
+                      onPressed: () {
+                        setState(() {
+                          // addNewTransaction(titleInput, amountInput);
+                          transactions.add(Transaction(
+                            id: DateTime.now().toString(),
+                            title: titleInput,
+                            amount: amountInput,
+                            timestamp: DateTime.now(),
+                          ));
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Column(
+              children: transactions
+                  .map((tx) => TxCard(
+                        id: tx.id,
+                        title: tx.title,
+                        amount: tx.amount,
+                        timestamp: tx.timestamp,
+                      ))
+                  .toList(),
+            )
+          ],
+        ),
       ),
     );
   }
